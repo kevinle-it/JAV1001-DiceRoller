5ed90bd285bcd632019f8f884c6fd863ec0a8141
Tri Le <trile95.it@gmail.com>
2022-04-14 18:38:47 -0400

    add all references to xml elements

--- app/src/main/java/com/trile/diceroller/MainActivity.java
+++ app/src/main/java/com/trile/diceroller/MainActivity.java
@@ -3,12 +3,38 @@ package com.trile.diceroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textRollResult;
+    Spinner spinnerDie;
+
+    Button btnRollOnce;
+    Button btnRollTwice;
+
+    EditText inputNumOfSides;
+    Button btnAddNewDie;
+
+    TextView textHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textRollResult = findViewById(R.id.textRollResult);
+        spinnerDie = findViewById(R.id.spinnerDie);
+
+        btnRollOnce = findViewById(R.id.btnRollOnce);
+        btnRollTwice = findViewById(R.id.btnRollTwice);
+
+        inputNumOfSides = findViewById(R.id.inputNumOfSides);
+        btnAddNewDie = findViewById(R.id.btnAddNewDie);
+
+        textHistory = findViewById(R.id.textHistory);
     }
 }
