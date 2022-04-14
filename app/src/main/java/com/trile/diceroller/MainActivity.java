859a76d9e09726975793ecdeba2b9d8610afb2a7
Tri Le <trile95.it@gmail.com>
2022-04-14 19:23:52 -0400

    setup spinner die

--- app/src/main/java/com/trile/diceroller/MainActivity.java
+++ app/src/main/java/com/trile/diceroller/MainActivity.java
@@ -3,15 +3,24 @@ package com.trile.diceroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView textRollResult;
     Spinner spinnerDie;
+    int[] spinnerDieOptions;
+
+    int minValue = 1, maxValue = 1;
 
     Button btnRollOnce;
     Button btnRollTwice;
@@ -36,5 +45,37 @@ public class MainActivity extends AppCompatActivity {
         btnAddNewDie = findViewById(R.id.btnAddNewDie);
 
         textHistory = findViewById(R.id.textHistory);
+
+        setupSpinner();
+    }
+
+    private void setupSpinner() {
+        spinnerDieOptions = getResources().getIntArray(R.array.die_options);
+
+        List<String> options = new ArrayList();
+        for (int option : spinnerDieOptions) {
+            options.add(String.valueOf(option));
+        }
+
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                options
+        );
+        spinnerDie.setAdapter(adapter);
+
+        minValue = 1;
+        maxValue = spinnerDieOptions[0];
+
+        spinnerDie.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                minValue = 1;
+                maxValue = spinnerDieOptions[i];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
     }
 }
