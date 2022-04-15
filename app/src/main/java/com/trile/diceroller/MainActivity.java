9cfd910adae874701f632cb76fac21a4c39c5d23
Tri Le <trile95.it@gmail.com>
2022-04-14 21:46:10 -0400

    enable adding new die type to spinner

--- app/src/main/java/com/trile/diceroller/MainActivity.java
+++ app/src/main/java/com/trile/diceroller/MainActivity.java
@@ -2,9 +2,15 @@ package com.trile.diceroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -14,12 +20,14 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textRollResult;
     Spinner spinnerDie;
-    int[] spinnerDieOptions;
+    List<Integer> spinnerDieOptions = new ArrayList();
+    ArrayAdapter<CharSequence> spinnerDieAdapter;
 
     int minValue = 1, maxValue = 1;
 
@@ -50,31 +58,37 @@ public class MainActivity extends AppCompatActivity {
 
         setupSpinner();
         setupButtonsRoll();
+        setupInputNumOfSides();
+        setupButtonAddNewDie();
     }
 
     private void setupSpinner() {
-        spinnerDieOptions = getResources().getIntArray(R.array.die_options);
+        int[] dieOptions = getResources().getIntArray(R.array.die_options);
+
+        for (int option : dieOptions) {
+            spinnerDieOptions.add(option);
+        }
 
         List<String> options = new ArrayList();
         for (int option : spinnerDieOptions) {
             options.add(String.valueOf(option));
         }
 
-        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(
+        spinnerDieAdapter = new ArrayAdapter(
                 this,
                 android.R.layout.simple_spinner_item,
                 options
         );
-        spinnerDie.setAdapter(adapter);
+        spinnerDie.setAdapter(spinnerDieAdapter);
 
         minValue = 1;
-        maxValue = spinnerDieOptions[0];
+        maxValue = spinnerDieOptions.get(0);
 
         spinnerDie.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 minValue = 1;
-                maxValue = spinnerDieOptions[i];
+                maxValue = spinnerDieOptions.get(i);
             }
 
             @Override
@@ -110,6 +124,59 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void setupInputNumOfSides() {
+        inputNumOfSides.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().equals("0")) {
+                    // 0 sided die is not allowed
+                    inputNumOfSides.setText("");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
+        inputNumOfSides.setOnEditorActionListener((textView, i, keyEvent) -> {
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                btnAddNewDie.callOnClick();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void setupButtonAddNewDie() {
+        btnAddNewDie.setOnClickListener(view -> {
+            // Close keyboard
+            hideSoftKeyboard((Activity) view.getContext(), true);
+
+            String dieTypeText = inputNumOfSides.getText().toString();
+            int dieTypeInt = parseInt(dieTypeText);
+
+            if (!spinnerDieOptions.contains(dieTypeInt)) {
+                // Add new item to spinner
+                spinnerDieAdapter.insert(dieTypeText,spinnerDieAdapter.getCount());
+                spinnerDieAdapter.notifyDataSetChanged();
+
+                spinnerDie.setSelection(spinnerDieAdapter.getCount() - 1);
+
+                // Add same new item to options list
+                spinnerDieOptions.add(dieTypeInt);
+            } else {
+                int duplicatedPos = spinnerDieOptions.indexOf(dieTypeInt);
+                spinnerDie.setSelection(duplicatedPos);
+            }
+
+            // Reset edit text
+            inputNumOfSides.setText("");
+        });
+    }
+
     private String buildHistoryRollResults(List<Integer> rollResults, Resources resources) {
         String listStr = rollResults.toString();
         return resources.getString(
@@ -125,5 +192,28 @@ public class MainActivity extends AppCompatActivity {
             max = temp;
         }
         return (int) (Math.random() * (max - min + 1)) + min;
+    }
+
+    private void hideSoftKeyboard(Activity activity, boolean clearCurrentFocus) {
+        Objects.requireNonNull(activity, "Cannot Hide Soft Keyboard with NULL Activity/Context!");
+        // Check if no view has focused.
+        View view = activity.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            if (clearCurrentFocus) {
+                view.clearFocus();
+            }
+        }
+    }
+
+    private int parseInt(String str) {
+        try{
+            return Integer.valueOf(str);
+        }
+        catch (NumberFormatException ex){
+            ex.printStackTrace();
+        }
+        return -1;
     }
 }
